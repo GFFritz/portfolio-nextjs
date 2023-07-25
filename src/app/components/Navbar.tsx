05a68f5835864defd1a533e26639801ff3f59f67
GFFritz <gabrielf.fritz@gmail.com>
2023-07-25 16:21:52 -0300

    Changes in buttons

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,9 +1,10 @@
+import { BrainCog, Home, Kanban, Layers, Mail, User } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 
 export function Navbar() {
   return (
-    <nav className="mx-auto my-5 hidden w-4/5 justify-between rounded bg-purple-700 bg-opacity-5 px-4 py-2 sm:flex">
+    <nav className="mx-auto my-5 hidden w-11/12 justify-between rounded bg-purple-700 bg-opacity-5 px-4 py-2 sm:flex">
       <div>
         <Image
           className="invert"
@@ -13,23 +14,47 @@ export function Navbar() {
           alt="Logo"
         />
       </div>
-      <div className="flex gap-3 font-medium text-zinc-300">
-        <Link href="" className="hover:text-purple-600">
+      <div className="flex gap-4 font-medium text-zinc-300">
+        <Link
+          href=""
+          className="flex items-center gap-1 transition-colors duration-500 hover:text-purple-600"
+        >
+          <Home size={15} />
           Home
         </Link>
-        <Link href="" className="hover:text-purple-600">
+        <Link
+          href=""
+          className="flex items-center gap-1 transition-colors duration-500 hover:text-purple-600"
+        >
+          <User size={15} />
           About me
         </Link>
-        <Link href="" className="hover:text-purple-600">
+        <Link
+          href=""
+          className="flex items-center gap-1 transition-colors duration-500 hover:text-purple-600"
+        >
+          <Kanban size={15} />
           Portfolio
         </Link>
-        <Link href="" className="hover:text-purple-600">
+        <Link
+          href=""
+          className="flex items-center gap-1 transition-colors duration-500 hover:text-purple-600"
+        >
+          <Layers size={15} />
           Skill
         </Link>
-        <Link href="" className="hover:text-purple-600">
+        <Link
+          href=""
+          className="flex items-center gap-1 transition-colors duration-500 hover:text-purple-600"
+        >
+          <BrainCog size={15} />
           Experience
         </Link>
-        <Link href="" className="hover:text-purple-600">
+        <Link
+          href=""
+          className="flex items-center gap-1 transition-colors duration-500 hover:text-purple-600"
+        >
+          <Mail size={15} />
           Contact
         </Link>
       </div>
