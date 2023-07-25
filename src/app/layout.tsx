177f6bcf3690c02af63b56a875ddad899763516e
GFFritz <gabrielf.fritz@gmail.com>
2023-07-25 15:20:50 -0300

    Color corrections

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={baiJamjuree.className}>{children}</body>
+      <body className={`${baiJamjuree.className} bg-ebony-950`}>
+        {children}
+      </body>
     </html>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,37 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        ebony: {
+          50: '#f3f8fa',
+          100: '#d8e5ef',
+          200: '#b0cadf',
+          300: '#81a5c7',
+          400: '#577faa',
+          500: '#3d648f',
+          600: '#2f4d72',
+          700: '#293f5c',
+          800: '#24354b',
+          900: '#222e3f',
+          950: '#090e16',
+        },
+        purple: {
+          50: '#f6f4fe',
+          100: '#edeafd',
+          200: '#ddd9fb',
+          300: '#c4baf8',
+          400: '#a792f3',
+          500: '#8966ec',
+          600: '#7b4ae2',
+          700: '#6934cd',
+          800: '#572bac',
+          900: '#49258d',
+          950: '#2d165f',
+        },
+      },
+
       fontFamily: {
         sans: 'var(--font-bai-jamjuree)',
-      },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
