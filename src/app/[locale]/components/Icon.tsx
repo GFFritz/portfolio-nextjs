b6e0e2fd2a3580b8a75715b0d24a4ab9aac54b48
GFFritz <gabrielf.fritz@gmail.com>
2023-08-03 16:35:42 -0300

    fix in tooltip

--- src/app/[locale]/components/Icon.tsx
+++ src/app/[locale]/components/Icon.tsx
@@ -15,7 +15,7 @@ const Icon = ({ icon, title }: IconProps) => {
         width={40}
         className="cursor-default hover:brightness-110"
       />
-      <span className="pointer-events-none absolute -bottom-9 -left-1 z-20 rounded bg-purple-500 px-2 text-zinc-300 opacity-0 transition-opacity group-hover:opacity-100">
+      <span className="pointer-events-none absolute -bottom-9 -left-1 z-20 w-max rounded bg-purple-500 px-2 text-zinc-300 opacity-0 transition-opacity group-hover:opacity-100">
         {title}
       </span>
     </div>
