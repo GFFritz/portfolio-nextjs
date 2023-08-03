48408cc7a22a30c6d23dd847189661fb426e022d
GFFritz <gabrielf.fritz@gmail.com>
2023-08-03 16:36:17 -0300

    Changes in projects section

---
+++ src/app/[locale]/components/Project.tsx
@@ -0,0 +1,48 @@
+import { Folder, Github, LinkIcon } from 'lucide-react'
+import Link from 'next/link'
+import Tag from './Tags'
+
+interface ProjectProps {
+  title: string
+  description: string
+  link?: string
+  github?: string
+  tags?: string[]
+}
+
+const Project = ({ title, description, link, github, tags }: ProjectProps) => {
+  return (
+    <div className="rounded bg-purple-800 bg-opacity-20 px-5 py-4 hover:bg-opacity-30">
+      <div className="flex justify-between text-purple-500">
+        {link && (
+          <div className="flex h-9 w-9 items-center justify-center rounded-full hover:bg-purple-800 hover:bg-opacity-25">
+            <Link href={link}>
+              <LinkIcon size={20} />
+            </Link>
+          </div>
+        )}
+
+        {github && (
+          <div className="flex h-9 w-9 items-center justify-center rounded-full hover:bg-purple-800 hover:bg-opacity-25">
+            <Link href={github}>
+              <Github size={20} />
+            </Link>
+          </div>
+        )}
+      </div>
+      <div className="mt-4 font-semibold text-zinc-300">{title}</div>
+      <div className="my-3 text-xs text-zinc-400">{description}</div>
+      <div className="flex flex-wrap gap-1">
+        {tags && tags.length > 0 && (
+          <div className="my-2 flex flex-wrap gap-1">
+            {tags.map((tag, index) => (
+              <Tag key={index} text={tag} />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Project
--- src/app/[locale]/components/Projects.tsx
+++ src/app/[locale]/components/Projects.tsx
@@ -1,5 +1,6 @@
 import { Folder, Github, Link } from 'lucide-react'
 import Tag from './Tags'
+import Project from './Project'
 
 export function Projects() {
   return (
@@ -7,63 +8,66 @@ export function Projects() {
       <h1 className="mb-4 text-center text-2xl font-medium text-zinc-100">
         Projects
       </h1>
+
+      <h2 className="mb-3 flex justify-center text-lg text-zinc-400">
+        Projetos Profissionais
+      </h2>
       <div className="grid grid-cols-2 gap-4 md:grid-cols-4">
-        <div className="rounded bg-purple-800 bg-opacity-20 px-5 py-4 hover:bg-opacity-30">
-          <div className="flex justify-between text-purple-500">
-            <div>
-              <Folder size={20} />
-            </div>
-            <div className="flex gap-1">
-              <Github size={20} />
-              <Link size={20} />
-            </div>
-          </div>
-          <div className="mt-4 font-semibold text-zinc-300">PROJECT NAME</div>
-          <div className="my-3 text-xs text-zinc-400">Project description</div>
-          <div className="flex flex-wrap gap-1">
-            <Tag text="NextJS" />
-            <Tag text="Tailwindcss" />
-            <Tag text="Typescript" />
-          </div>
-        </div>
+        <Project
+          title="AlertaCar"
+          link="https://alertacar.com.br"
+          description="Projeto de integração com whatsapp e APIs terceiras e ERP, utilizando SOAP e REST"
+          tags={['Reacjs', 'Laravel 8', 'Node', 'REST API', 'SOAP', 'Sockets']}
+        />
 
-        <div className="rounded bg-purple-800 bg-opacity-20 px-5 py-4 shadow-purple-400 hover:bg-opacity-30">
-          <div className="flex justify-between text-purple-500">
-            <div>
-              <Folder size={20} />
-            </div>
-            <div className="flex gap-1">
-              <Github size={20} />
-              <Link size={20} />
-            </div>
-          </div>
-          <div className="mt-4 font-semibold text-zinc-300">PROJECT NAME</div>
-          <div className="my-3 text-xs text-zinc-400">Project description</div>
-          <div className="flex flex-wrap gap-1">
-            <Tag text="NextJS" />
-            <Tag text="Tailwindcss" />
-            <Tag text="Typescript" />
-          </div>
-        </div>
+        <Project
+          link="https://indikeganhe.com.br"
+          title="IndikeGanhe"
+          description="Painel com integrações de CRMs e ERP, utilizando REST"
+          tags={['Reacjs', 'PHP', 'Laravel 8', 'REST API']}
+        />
 
-        <div className="rounded bg-purple-800 bg-opacity-20 px-5 py-4 shadow-purple-400 hover:bg-opacity-30">
-          <div className="flex justify-between text-purple-500">
-            <div>
-              <Folder size={20} />
-            </div>
-            <div className="flex gap-1">
-              <Github size={20} />
-              <Link size={20} />
-            </div>
-          </div>
-          <div className="mt-4 font-semibold text-zinc-300">PROJECT NAME</div>
-          <div className="my-3 text-xs text-zinc-400">Project description</div>
-          <div className="flex flex-wrap gap-1">
-            <Tag text="NextJS" />
-            <Tag text="Tailwindcss" />
-            <Tag text="Typescript" />
-          </div>
-        </div>
+        <Project
+          link="https://credencieconsultas.com.br"
+          title="Pefin"
+          description="Painel com integração de API própria e APIs de terceiros"
+          tags={['Slim', 'PHP', 'Twig', 'Node']}
+        />
+
+        <Project
+          title="Credencie Digital"
+          description="Ecommerce, integrado com APIs de terceiros"
+          tags={['Opencart', 'Lumen', 'PHP', 'Boostrap', 'REST API']}
+        />
+      </div>
+
+      <hr className="mx-auto my-10 flex w-2/4 rounded-full border-emerald-600 opacity-30" />
+
+      <h2 className="mb-3 flex justify-center text-lg text-zinc-400">
+        Projetos Pessoais
+      </h2>
+      <div className="grid grid-cols-2 gap-4 md:grid-cols-4">
+        <Project
+          title="AlertaCar"
+          link="https://alertacar.com.br"
+          description="Project Description"
+          tags={['Reacjs', 'Laravel 8', 'REST API']}
+        />
+
+        <Project
+          link="https://alertacar.com.br"
+          github="http://teste.com"
+          title="IndikeGanhe"
+          description="Project Description"
+          tags={['Reacjs', 'Laravel 8', 'REST API']}
+        />
+
+        <Project
+          link="https://alertacar.com.br"
+          title="AlertaCar"
+          description="Project Description"
+          tags={['Reacjs', 'Laravel 8', 'REST API']}
+        />
       </div>
     </div>
   )
