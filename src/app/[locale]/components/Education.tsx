82c06b1e05be5419039fd8ac6890529b20a137ca
GFFritz <gabrielf.fritz@gmail.com>
2023-08-04 00:04:49 -0300

    dynamization of education section with translate

--- src/app/[locale]/components/Education.tsx
+++ src/app/[locale]/components/Education.tsx
@@ -1,17 +1,36 @@
 import Timeline from './Timeline'
+import { useTranslations } from 'next-intl'
+
+interface CourseData {
+  period: string
+  title: string
+  subtitle: string
+  description: string
+}
+
+interface CoursesData {
+  [key: string]: CourseData
+}
 
 export function Education() {
+  const t = useTranslations('Education')
+  const courses: CoursesData = t.raw('courses')
+
   return (
     <div className="mx-auto flex w-full flex-col items-center gap-4">
       <h1 className="mb-4 text-center text-2xl font-medium text-zinc-100">
         Education
       </h1>
-      <Timeline
-        period="MAR 2016 - AUG 2017"
-        title="Curso Técnico de TI"
-        subtitle="QI escolas e faculdades"
-        text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, nobis suscipit officia voluptate dicta at id, maiores aut eius nostrum facilis exercitationem dignissimos autem. Veritatis optio possimus nesciunt odio similique."
-      />
+
+      {Object.values(courses).map((courseData, index) => (
+        <Timeline
+          key={index}
+          period={courseData.period}
+          title={courseData.title}
+          subtitle={courseData.subtitle}
+          text={courseData.description}
+        />
+      ))}
     </div>
   )
 }
