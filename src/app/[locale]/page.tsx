785e27e8f6322301c1bc0f50a59232800d6a192a
GFFritz <gabrielf.fritz@gmail.com>
2023-08-04 00:28:37 -0300

    Removed desktop navbar

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,7 +4,6 @@ import { Divider } from './components/Divider'
 import { Education } from './components/Education'
 import { Experiences } from './components/Experiences'
 import { Hero } from './components/Hero'
-import { Navbar } from './components/Navbar'
 import { Projects } from './components/Projects'
 import { Skills } from './components/Skills'
 
@@ -16,7 +15,6 @@ export default function Home() {
   return (
     <main>
       <header>
-        <Navbar />
         <Hero />
         <About />
         <Divider />
