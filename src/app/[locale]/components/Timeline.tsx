a45d5a59d8a5279b3cc881d847164c1476db1217
GFFritz <gabrielf.fritz@gmail.com>
2023-08-03 23:47:26 -0300

    Fixed responsive issues in timeline

--- src/app/[locale]/components/Timeline.tsx
+++ src/app/[locale]/components/Timeline.tsx
@@ -10,25 +10,28 @@ interface TimelineProps {
 
 const Timeline = ({ period, title, subtitle, text, tags }: TimelineProps) => {
   return (
-    <div className="block w-4/5 gap-5 md:w-2/4 lg:flex xl:w-2/5">
+    <div className="xl:max block w-4/5 justify-center gap-5 rounded px-5 py-4 hover:bg-purple-700 hover:bg-opacity-5 md:w-2/4 lg:flex">
       <div className="w-2/3 text-sm text-zinc-400 lg:w-1/3 xl:w-2/5 2xl:w-1/5">
         {period}
       </div>
-      <div className="lg:w-5/6">
+      <div className="lg:w-5/6 2xl:w-2/4">
         <div className="">
           <h1 className="font-semibold uppercase text-zinc-200">{title}</h1>
           {subtitle && (
-            <h1 className="text-xs font-semibold uppercase text-zinc-400">
+            <h1 className="mb-3 text-xs font-semibold uppercase text-zinc-400">
               {subtitle}
             </h1>
           )}
         </div>
         <span className="text-sm text-zinc-300">{text}</span>
         {tags && tags.length > 0 && (
-          <div className="my-2 flex flex-wrap gap-1">
-            {tags.map((tag, index) => (
-              <Tag key={index} text={tag} />
-            ))}
+          <div>
+            <h1 className="mt-3 text-xs text-zinc-400">Stacks:</h1>
+            <div className="my-2 flex flex-wrap gap-1">
+              {tags.map((tag, index) => (
+                <Tag key={index} text={tag} />
+              ))}
+            </div>
           </div>
         )}
       </div>
