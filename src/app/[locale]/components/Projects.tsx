a6df93f85891b5993b87508722d8992820f6623d
GFFritz <gabrielf.fritz@gmail.com>
2023-08-04 00:01:43 -0300

    dynamization of projects section with translate

--- src/app/[locale]/components/Projects.tsx
+++ src/app/[locale]/components/Projects.tsx
@@ -1,47 +1,45 @@
-import { Folder, Github, Link } from 'lucide-react'
-import Tag from './Tags'
+import { useTranslations } from 'next-intl'
 import Project from './Project'
 
+interface ProjectData {
+  title: string
+  link: string
+  github?: string
+  description: string
+  stacks: string[]
+}
+
+interface ProjectsData {
+  [key: string]: ProjectData
+}
+
 export function Projects() {
+  const t = useTranslations('Projects')
+  const project: ProjectsData = t.raw('pro')
+
   return (
     <div className="mx-auto w-4/5">
       <h1 className="mb-4 text-center text-2xl font-medium text-zinc-100">
-        Projects
+        {t('title')}
       </h1>
 
       <h2 className="mb-3 flex justify-center text-lg text-zinc-400">
-        Projetos Profissionais
+        {t('pro-title')}
       </h2>
       <div className="grid grid-cols-2 gap-4 md:grid-cols-4">
-        <Project
-          title="AlertaCar"
-          link="https://alertacar.com.br"
-          description="Projeto de integração com whatsapp e APIs terceiras e ERP, utilizando SOAP e REST"
-          tags={['Reacjs', 'Laravel 8', 'Node', 'REST API', 'SOAP', 'Sockets']}
-        />
-
-        <Project
-          link="https://indikeganhe.com.br"
-          title="IndikeGanhe"
-          description="Painel com integrações de CRMs e ERP, utilizando REST"
-          tags={['Reacjs', 'PHP', 'Laravel 8', 'REST API']}
-        />
-
-        <Project
-          link="https://credencieconsultas.com.br"
-          title="Pefin"
-          description="Painel com integração de API própria e APIs de terceiros"
-          tags={['Slim', 'PHP', 'Twig', 'Node']}
-        />
-
-        <Project
-          title="Credencie Digital"
-          description="Ecommerce, integrado com APIs de terceiros"
-          tags={['Opencart', 'Lumen', 'PHP', 'Boostrap', 'REST API']}
-        />
+        {Object.values(project).map((projectData, index) => (
+          <Project
+            key={index}
+            title={projectData.title}
+            link={projectData.link}
+            github={projectData.github}
+            description={projectData.description}
+            tags={projectData.stacks}
+          />
+        ))}
       </div>
 
-      <hr className="mx-auto my-10 flex w-2/4 rounded-full border-emerald-600 opacity-30" />
+      {/* <hr className="mx-auto my-10 flex w-2/4 rounded-full border-emerald-600 opacity-30" />
 
       <h2 className="mb-3 flex justify-center text-lg text-zinc-400">
         Projetos Pessoais
@@ -68,7 +66,7 @@ export function Projects() {
           description="Project Description"
           tags={['Reacjs', 'Laravel 8', 'REST API']}
         />
-      </div>
+      </div> */}
     </div>
   )
 }
