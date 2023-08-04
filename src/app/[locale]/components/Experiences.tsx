5774b2659898c51b0932adddfbe639d94e0d9c30
GFFritz <gabrielf.fritz@gmail.com>
2023-08-03 23:51:44 -0300

    dynamization of experience section with translate

--- src/app/[locale]/components/Experiences.tsx
+++ src/app/[locale]/components/Experiences.tsx
@@ -1,70 +1,38 @@
 import Timeline from './Timeline'
+import { useTranslations } from 'next-intl'
+
+interface ExperienceData {
+  period: string
+  title: string
+  subtitle: string
+  description: string
+  stacks: string[]
+}
+
+interface ExperiencesData {
+  [key: string]: ExperienceData
+}
 
 export function Experiences() {
+  const t = useTranslations('Experiences')
+  const exp: ExperiencesData = t.raw('exp')
+
   return (
     <div className="mx-auto flex w-full flex-col items-center gap-4">
       <h1 className="mb-4 text-center text-2xl font-medium text-zinc-100">
-        Experiences
+        {t('title')}
       </h1>
 
-      <Timeline
-        period="JAN 2021 - MAR 2023"
-        title="Credencie Consultas Cadastrais"
-        subtitle="Fullstack Developer"
-        text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
-        tempore delectus assumenda voluptas minus odit inventore laborum ut
-        nostrum ipsam, sed, quasi incidunt. Nisi harum dolor maiores
-        blanditiis. Iusto, numquam."
-        tags={[
-          'Tailwindcss',
-          'Reacjs',
-          'Laravel 8',
-          'Slim',
-          'Codeigniter',
-          'Opencart',
-          'Git',
-          'Linux',
-          'AWS',
-          'PHP',
-          'Boostrap',
-          'Node.js',
-          'TypeScript',
-          'MongoDB',
-          'REST API',
-        ]}
-      />
-
-      <Timeline
-        period="DEZ 2019 - DEZ 2020"
-        title="Bdi brasil e-business digital"
-        subtitle="Front-end Developer"
-        text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
-        tempore delectus assumenda voluptas minus odit inventore laborum ut
-        nostrum ipsam, sed, quasi incidunt. Nisi harum dolor maiores
-        blanditiis. Iusto, numquam."
-        tags={['HTML', 'CSS', 'MySQL', 'Office']}
-      />
-
-      <Timeline
-        period="JAN 2018 - APR 2018"
-        title="N49 negócios digitais"
-        subtitle="AR Manager"
-        text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
-        tempore delectus assumenda voluptas minus odit inventore laborum ut
-        nostrum ipsam, sed, quasi incidunt. Nisi harum dolor maiores
-        blanditiis. Iusto, numquam."
-        tags={[
-          'HTML',
-          'CSS',
-          'MySQL',
-          'PHP',
-          'Bootstrap',
-          'SASS',
-          'Javascript',
-          'Codeigniter',
-          'Opencart',
-        ]}
-      />
+      {Object.values(exp).map((expData, index) => (
+        <Timeline
+          key={index}
+          period={expData.period}
+          title={expData.title}
+          subtitle={expData.subtitle}
+          text={expData.description}
+          tags={expData.stacks}
+        />
+      ))}
     </div>
   )
 }
