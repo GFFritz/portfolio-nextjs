6aa2077d1208d8a9b2c7f1b757f735f2b437a122
GFFritz <gabrielf.fritz@gmail.com>
2023-08-07 15:44:26 -0300

    New buttons to change lang

--- src/app/[locale]/components/Hero.tsx
+++ src/app/[locale]/components/Hero.tsx
@@ -1,13 +1,53 @@
 import { Linkedin, Github, Download, Mail, Code2 } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
+import LinkLang from 'next-intl/link'
 import { useTranslations } from 'next-intl'
 
 export function Hero() {
   const t = useTranslations('About')
   return (
     <div>
-      <div className="mt-4 flex items-center justify-center gap-4">
+      <div className="my-4 mb-4 flex items-center justify-center gap-4 font-semibold text-zinc-200">
+        <div className="group relative w-max">
+          <LinkLang className="hover:opacity-80" href="/" locale="en">
+            <Image
+              src="images/icons/us_icon.svg"
+              width={30}
+              height={30}
+              alt="US Flag"
+            />
+          </LinkLang>
+          <span className="pointer-events-none absolute -bottom-9 -left-6 w-max rounded bg-purple-500 px-2 text-zinc-300 opacity-0 transition-opacity group-hover:opacity-100">
+            In English
+          </span>
+        </div>
+
+        <div className="group relative w-max">
+          <LinkLang className="hover:opacity-80" href="/" locale="pt">
+            <Image
+              src="images/icons/br_icon.svg"
+              width={30}
+              height={30}
+              alt="Brazil Flag"
+            />
+          </LinkLang>
+          <span className="pointer-events-none absolute -bottom-9 -left-11 w-max rounded bg-purple-500 px-2 text-zinc-300 opacity-0 transition-opacity group-hover:opacity-100">
+            Em Português
+          </span>
+        </div>
+      </div>
+
+      <div className="mb-4 mt-4 flex justify-center">
+        <Image
+          src="/images/gabriel_avatar.png"
+          width={300}
+          height={300}
+          alt="Avatar Gabriel"
+        />
+      </div>
+
+      <div className="mb-4 flex items-center justify-center gap-4">
         <div className="group relative w-max">
           <Link href="https://www.linkedin.com/in/gffritz/">
             <Linkedin className="text-zinc-400 transition-all duration-500 hover:text-purple-500" />
@@ -25,15 +65,6 @@ export function Hero() {
             Github
           </span>
         </div>
-      </div>
-
-      <div className="my-8 flex justify-center">
-        <Image
-          src="/images/gabriel_avatar.png"
-          width={300}
-          height={300}
-          alt="Avatar Gabriel"
-        />
       </div>
 
       <div>
