b6ab0f7165f1730813d31fa159d5f5ffeff3d7a4
GFFritz <gabrielf.fritz@gmail.com>
2024-01-15 17:54:53 -0300

    Changed size of the elements

--- src/app/[locale]/components/Timeline.tsx
+++ src/app/[locale]/components/Timeline.tsx
@@ -10,11 +10,11 @@ interface TimelineProps {
 
 const Timeline = ({ period, title, subtitle, text, tags }: TimelineProps) => {
   return (
-    <div className="xl:max block w-4/5 justify-center gap-5 rounded px-5 py-4 hover:bg-purple-700 hover:bg-opacity-5 md:w-2/4 lg:flex">
-      <div className="flex whitespace-nowrap text-sm text-zinc-400 lg:text-end">
+    <div className="block w-4/5 justify-center gap-5 rounded px-5 py-4 hover:bg-purple-700 hover:bg-opacity-5 md:w-3/5 lg:flex">
+      <div className="flex whitespace-nowrap text-sm text-zinc-400 lg:text-end 2xl:w-1/4">
         {period}
       </div>
-      <div className="lg:w-5/6 2xl:w-2/4">
+      <div className="lg:w-5/6 2xl:w-3/4">
         <div className="">
           <h1 className="font-semibold uppercase text-zinc-200">{title}</h1>
           {subtitle && (
