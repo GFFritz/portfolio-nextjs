677ba77d51134bfb51ceb6146b4aeb0abe612b72
GFFritz <gabrielf.fritz@gmail.com>
2024-01-15 18:10:30 -0300

    Changed period orientation

--- src/app/[locale]/components/Timeline.tsx
+++ src/app/[locale]/components/Timeline.tsx
@@ -11,7 +11,7 @@ interface TimelineProps {
 const Timeline = ({ period, title, subtitle, text, tags }: TimelineProps) => {
   return (
     <div className="block w-4/5 justify-center gap-5 rounded px-5 py-4 hover:bg-purple-700 hover:bg-opacity-5 md:w-3/5 lg:flex">
-      <div className="flex whitespace-nowrap text-sm text-zinc-400 lg:text-end 2xl:w-1/4">
+      <div className="flex whitespace-nowrap text-sm text-zinc-400 lg:justify-end 2xl:w-1/4">
         {period}
       </div>
       <div className="lg:w-5/6 2xl:w-3/4">
