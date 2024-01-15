0906c8380e805e7f4d4774ef25611b1d904e5758
GFFritz <gabrielf.fritz@gmail.com>
2024-01-15 17:54:22 -0300

    Changed objects order

--- src/app/[locale]/components/Experiences.tsx
+++ src/app/[locale]/components/Experiences.tsx
@@ -17,20 +17,29 @@ export function Experiences() {
   const t = useTranslations('Experiences')
   const exp: ExperiencesData = t.raw('exp')
 
+  // Função para ordenar os projetos em ordem decrescente com base no índice
+  const sortByIndex = (
+    a: [string, ExperienceData],
+    b: [string, ExperienceData],
+  ) => parseInt(b[0]) - parseInt(a[0])
+
+  // Ordena os projetos em ordem decrescente
+  const orderedExperiences = Object.entries(exp).sort(sortByIndex)
+
   return (
     <div className="mx-auto flex w-full flex-col items-center gap-4">
       <h1 className="mb-4 text-center text-2xl font-medium text-zinc-100">
         {t('title')}
       </h1>
 
-      {Object.values(exp).map((expData, index) => (
+      {orderedExperiences.map(([index, ExperienceData]) => (
         <Timeline
           key={index}
-          period={expData.period}
-          title={expData.title}
-          subtitle={expData.subtitle}
-          text={expData.description}
-          tags={expData.stacks}
+          period={ExperienceData.period}
+          title={ExperienceData.title}
+          subtitle={ExperienceData.subtitle}
+          text={ExperienceData.description}
+          tags={ExperienceData.stacks}
         />
       ))}
     </div>
